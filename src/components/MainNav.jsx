15eb4ba7ffd681a2626c1b6617a0cc92057daf10
Ali Zia <ali.zia.1991@gmail.com>
2025-05-15 21:39:18 +0500

    ui changes

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AiOutlineUser } from "react-icons/ai";
 import { Link } from 'react-router-dom';
 import Logo from '../../src/images/logo.png';
@@ -7,16 +7,39 @@ import { IoIosSearch } from "react-icons/io";
 import { CiHeart } from "react-icons/ci";
 import { useCart } from "../contexts/CartContext";
 import { FaHeart,FaEye } from "react-icons/fa";
-
+import { CiMenuFries } from "react-icons/ci";
+import { IoCloseCircleOutline } from "react-icons/io5";
 
 
 const MainNav = () => {
 
   const { wishlist, carts } = useCart();
-
+const [showMenu,setShowMenu] = useState(false)
 
   return (
     <div className='main-search-wrapper'>
+     {showMenu &&  <div className="mobile-menu-wrapper">
+       <div className="close-icon" onClick={()=>setShowMenu(false)}>
+        <IoCloseCircleOutline />
+       </div>
+       <h2 className="title">
+        About
+       </h2>
+       <Link to='/about'  onClick={()=>setShowMenu(false)}>About us</Link>
+                       <Link to='/advertise'  onClick={()=>setShowMenu(false)}>Advertise with us</Link>
+                       <Link to='/'  onClick={()=>setShowMenu(false)}>Delivery Information</Link>
+                       <Link to='/privacypolicy'  onClick={()=>setShowMenu(false)}>Privacy Policy</Link>
+                       <Link to='/termsandconditions'  onClick={()=>setShowMenu(false)}>Terms & Conditions</Link>
+                       <Link to='/contact-us'  onClick={()=>setShowMenu(false)}>Contact Us</Link>
+                       <h2 className="title">
+       My Account
+       </h2>
+        <Link to='/login'  onClick={()=>setShowMenu(false)}>Sign in</Link>
+                       <Link to='/register'  onClick={()=>setShowMenu(false)}>Register</Link>
+                       <Link to='/cart'  onClick={()=>setShowMenu(false)}>View cart</Link>
+                       
+                       <Link to='/blog'  onClick={()=>setShowMenu(false)}>Blog</Link>
+      </div>}
       
     <div className="container">
         <div className="inner">
@@ -59,6 +82,9 @@ const MainNav = () => {
       <AiOutlineUser  />
       <Link to='/login' className="name">Login/Signup</Link>
         </button>
+        <div className="mobile-menu" onClick={()=>setShowMenu((prev)=> !prev)}>
+          <CiMenuFries />
+        </div>
             </div>
            
         </div>
